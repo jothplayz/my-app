fd0b514619071cc46caf2b813667f2d6ddaab155
JiannaBixho <jiannabixho@gmail.com>
2025-04-09 08:31:02 -0400

    Style to requriement page

--- src/pages/addrequirement.js
+++ src/pages/addrequirement.js
@@ -3,7 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function BasicExample() {
   return (
-    <p>Requirement Page</p>
+    <p class = "title">What Type Of Requirement Would You Like To Make?</p>
+    
   );
 }
 
