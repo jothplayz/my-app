6c733ec5193dbf515ea9635635e2a2fccd9e6a05
JiannaBixho <jiannabixho@gmail.com>
2025-04-09 08:40:31 -0400

    Add style to requirement + added dropdown to req

--- src/pages/addrequirement.js
+++ src/pages/addrequirement.js
@@ -1,10 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function BasicExample() {
   return (
+    <div>
     <p class = "title">What Type Of Requirement Would You Like To Make?</p>
-    
+    <Dropdown class = "dropdownstyle">
+    <Dropdown.Toggle variant="success" id="dropdown-basic">
+      Dropdown Button
+    </Dropdown.Toggle>
+
+    <Dropdown.Menu>
+      <Dropdown.Item href="#/action-1">All School</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">Selected Days</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">Etc</Dropdown.Item>
+    </Dropdown.Menu>
+  </Dropdown>
+  </div>
   );
 }
 
