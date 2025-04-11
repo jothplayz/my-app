a1e87005e423721f5049935102ca990710d2fab4
JiannaBixho <jiannabixho@gmail.com>
2025-04-11 09:26:56 -0400

    Dropdown customization

--- src/pages/addrequirement.js
+++ src/pages/addrequirement.js
@@ -1,21 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Dropdown from 'react-bootstrap/Dropdown';
-
+import DropdownButton from 'react-bootstrap/DropdownButton'; //Allows for custimization of the dropdown
 function BasicExample() {
   return (
     <div>
     <p class = "title">What Type Of Requirement Would You Like To Make?</p>
-    <Dropdown class = "dropdownstyle">
-    <Dropdown.Toggle variant="success" id="dropdown-basic">
-      Dropdown Button
-    </Dropdown.Toggle>
-
-    <Dropdown.Menu>
-      <Dropdown.Item href="#/action-1">All School</Dropdown.Item>
-      <Dropdown.Item href="#/action-2">Selected Days</Dropdown.Item>
-      <Dropdown.Item href="#/action-3">Etc</Dropdown.Item>
-    </Dropdown.Menu>
-  </Dropdown>
+    <DropdownButton
+     id="dropdown-basic-button" 
+     title="Select..."
+      size="lg"
+    >
+      <Dropdown.Item href="#/action-1">All-School</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">Unavailible</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">Specifc Section Request</Dropdown.Item>
+      <Dropdown.Item href="#/action-4">Miscellaneuos</Dropdown.Item>
+    </DropdownButton>
   </div>
   );
 }
