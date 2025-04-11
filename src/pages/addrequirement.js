8ec31cd46f9473fbb4748fdf8a3e2d84a036979f
JiannaBixho <jiannabixho@gmail.com>
2025-04-11 09:43:35 -0400

    Created new function
    it doesnt show anything yet but im not getting compile errors

--- src/pages/addrequirement.js
+++ src/pages/addrequirement.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton'; //Allows for custimization of the dropdown
-function BasicExample() {
+
+function FirstReqPage() {
   return (
     <div>
     <p class = "title">What Type Of Requirement Would You Like To Make?</p>
+    <div class = "dropdownstyle">
     <DropdownButton
      id="dropdown-basic-button" 
      title="Select..."
@@ -15,8 +17,27 @@ function BasicExample() {
       <Dropdown.Item href="#/action-3">Specifc Section Request</Dropdown.Item>
       <Dropdown.Item href="#/action-4">Miscellaneuos</Dropdown.Item>
     </DropdownButton>
+    </div>
   </div>
   );
 }
+function AllSchool(){
+return (
+  <div>
+    <p class = "title">All-School ____</p>
+    <div class = "dropdownstyle">
+    <DropdownButton
+     id="dropdown-basic-button" 
+     title="Reason..."
+      size="lg"
+    >
+      <Dropdown.Item href="#/action-1">Class Project</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">Assessment</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">Other</Dropdown.Item>
+    </DropdownButton>
+    </div>
+  </div>
+);
+}
 
-export default BasicExample;
+export default FirstReqPage;
